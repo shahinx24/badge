c57951f303d7caf148d9406277a6f27a611e8618
Shahin <shahinofficial911@gmail.com>
2026-02-18 12:18:58 +0530

    update

--- traffic_sample/src/components/Traffic.jsx
+++ traffic_sample/src/components/Traffic.jsx
@@ -1,25 +1,26 @@
 import { useState, useEffect } from "react";
-import "./traffic.css"
+import "./traffic.css";
 
-export default function Traffic(){
+export default function Traffic() {
     const [light, setLight] = useState("red");
 
-    useEffect(()=>{
-        const intreval = setInterval(()=>{
-            setLight((prev)=>{
-                if(prev === "red") return "green";
-                if(prev === "green") return "yellow";
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setLight((prev) => {
+                if (prev === "red") return "green";
+                if (prev === "green") return "yellow";
                 return "red";
-            })
-        },3000)
-        return ()=> clearInterval(intreval);
-    },[])
+            });
+        }, 3000);
 
-    return(
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
         <div className="traffic-container">
-            <div className={`light red ${light === "red" ? "active" : ""}`}/>
-            <div className={`light yellow ${light === "yellow" ? "active" : ""}`}/>
-            <div className={`light green ${light === "green" ? "active" : ""}`}/>
+            <div className={`light red ${light === "red" ? "active" : ""}`} />
+            <div className={`light yellow ${light === "yellow" ? "active" : ""}`} />
+            <div className={`light green ${light === "green" ? "active" : ""}`} />
         </div>
-    )
+    );
 }
