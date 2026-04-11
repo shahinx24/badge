22c792984f5f896ae791cb79f9863b3799bfc60e
shahinx24 <shahinu886@gmail.com>
2026-04-11 13:15:34 +0530

    update

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,6 @@
 import './App.css'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import CreateBlog from './components/CreateBlog'
-import DetailBlog from './components/DetailBlog'
-import Login from './components/Login'
-import ViewBlog from './components/ViewBlog'
 import useBlogAppState from './hooks/useBlogAppState'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   const {
@@ -17,52 +13,14 @@ function App() {
   } = useBlogAppState()
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Login currentUser={currentUser} onLogin={setCurrentUser} />}
-      />
-      <Route
-        path="/create"
-        element={
-          currentUser ? (
-            <CreateBlog currentUser={currentUser} setBlogs={setBlogs} />
-          ) : (
-            <Navigate replace to="/" />
-          )
-        }
-      />
-      <Route
-        path="/view"
-        element={
-          currentUser ? (
-            <ViewBlog
-              blogs={blogs}
-              currentUser={currentUser}
-              onDeleteBlog={handleDeleteBlog}
-              onLogout={handleLogout}
-            />
-          ) : (
-            <Navigate replace to="/" />
-          )
-        }
-      />
-      <Route
-        path="/detail/:id"
-        element={
-          currentUser ? (
-            <DetailBlog
-              blogs={blogs}
-              currentUser={currentUser}
-              onDeleteBlog={handleDeleteBlog}
-            />
-          ) : (
-            <Navigate replace to="/" />
-          )
-        }
-      />
-      <Route path="*" element={<Navigate replace to={currentUser ? '/view' : '/'} />} />
-    </Routes>
+    <AppRoutes
+      blogs={blogs}
+      currentUser={currentUser}
+      onDeleteBlog={handleDeleteBlog}
+      onLogin={setCurrentUser}
+      onLogout={handleLogout}
+      setBlogs={setBlogs}
+    />
   )
 }
 
---
+++ blog/src/routes/AppRoutes.jsx
@@ -0,0 +1,60 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import CreateBlog from '../components/CreateBlog'
+import DetailBlog from '../components/DetailBlog'
+import Login from '../components/Login'
+import ViewBlog from '../components/ViewBlog'
+
+export default function AppRoutes({
+  blogs,
+  currentUser,
+  onDeleteBlog,
+  onLogin,
+  onLogout,
+  setBlogs,
+}) {
+  return (
+    <Routes>
+      <Route path="/" element={<Login currentUser={currentUser} onLogin={onLogin} />} />
+      <Route
+        path="/create"
+        element={
+          currentUser ? (
+            <CreateBlog currentUser={currentUser} setBlogs={setBlogs} />
+          ) : (
+            <Navigate replace to="/" />
+          )
+        }
+      />
+      <Route
+        path="/view"
+        element={
+          currentUser ? (
+            <ViewBlog
+              blogs={blogs}
+              currentUser={currentUser}
+              onDeleteBlog={onDeleteBlog}
+              onLogout={onLogout}
+            />
+          ) : (
+            <Navigate replace to="/" />
+          )
+        }
+      />
+      <Route
+        path="/detail/:id"
+        element={
+          currentUser ? (
+            <DetailBlog
+              blogs={blogs}
+              currentUser={currentUser}
+              onDeleteBlog={onDeleteBlog}
+            />
+          ) : (
+            <Navigate replace to="/" />
+          )
+        }
+      />
+      <Route path="*" element={<Navigate replace to={currentUser ? '/view' : '/'} />} />
+    </Routes>
+  )
+}
